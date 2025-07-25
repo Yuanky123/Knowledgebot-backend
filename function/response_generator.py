d910537ccf7b9cf670a22f1eafa17074ee76b46a
meow-wwww <wangxiaoyu2001@pku.edu.cn>
2025-07-25 22:14:00 +0800

    Style: delegating completed

--- function/response_generator.py
+++ function/response_generator.py
@@ -9,6 +9,22 @@ class ResponseGenerator:
     
     def generate_custom_response(self, context, strategy):
         """根据策略生成回复"""
+
+        intervention_style = context['style'] # 0: telling, 1: selling, 2: participating, 3: delegating
+        current_phase = context['phase']
+
+        if intervention_style == 0:
+            pass
+            intervention_message = ''
+        elif intervention_style == 1:
+            pass
+            intervention_message = ''
+        elif intervention_style == 2:
+            pass
+            intervention_message = ''
+        elif intervention_style == 3: # delegating
+            intervention_message = strategy
+
         response = {
             'body': 'test reply',
             'post_id': context['post']['id'],
